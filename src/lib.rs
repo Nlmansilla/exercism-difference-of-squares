2496c308a48c8fd5e749d511827bacd130197579
Nicolas Mansilla <nlmansilla89@gmail.com>
2024-08-26 22:03:54 +0100

    Changed loop cycles for aritmethic functions. Square of sums and sum of squares

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,10 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let mut sum: u32 = 0;
-    for i in 1..=n {
-        sum += i;
-    }
+    let sum: u32 = (n * ( n + 1 )) / 2;
     sum.pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    let mut sum: u32 = 0;
-    for i in 1..=n {
-        sum += i.pow(2);
-    }
-    sum
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
